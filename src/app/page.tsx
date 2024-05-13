8946a2c3d3c706957c4485ff2bb6930c0ef491bb
Deepak <deepak2603dk@gmail.com>
2024-05-14 03:57:08 +0530

    fixing light bloom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Hero from "./_Components/herosection/Hero/page";
 import Navbar from "./_Components/herosection/Navbar/page";
 import Stats from "./_Components/stats/page";
 import VioletBloom from "./_Components/smallcomponents/light/Light";
+import YellowBloom from "./_Components/smallcomponents/light/Light2";
 
 export default function Home() {
   return (
     <>
-      <div className=" relative h-full w-full bg-black">
+      <div className=" relative h-full w-full bg-black overflow-hidden">
+        <YellowBloom/>
         <Hero />
         <VioletBloom/>
        <Stats/>
