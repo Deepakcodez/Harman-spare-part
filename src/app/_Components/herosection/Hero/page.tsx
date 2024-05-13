2eb14ec0081ba44fa97ec2220bb54124483bcb46
Deepak <deepak2603dk@gmail.com>
2024-05-14 03:58:08 +0530

    adding bloom

--- src/app/_Components/herosection/Hero/page.tsx
+++ src/app/_Components/herosection/Hero/page.tsx
@@ -5,7 +5,7 @@ import Helemt3d from '../../3d/helmet';
 const Hero = () => {
     return ( 
         <>
-        <div className="h-screen w-full bg-black  ">
+        <div className=" relative z-0 h-screen w-full bg-black  ">
             <div className="bg-orange-50 w-full h-screen rounded-3xl ">
           <Helemt3d/>
             </div>
--- src/app/_Components/stats/page.tsx
+++ src/app/_Components/stats/page.tsx
@@ -6,21 +6,21 @@ const Stats = () => {
             <div className=" w-full h-[5rem] md:border-r text-center  md:border-r-yellow-200 flex flex-col items-center">
                 <div className="">
                 <h1 className="text-3xl font-bold w-[15rem] ">2000+</h1>
-                <h1 className="text-violet-100 ">Happy Customers</h1>
+                <h1 className="text-violet-100 font-thin">Happy Customers</h1>
                 </div>
             </div>
 
             <div className=" w-full h-[5rem] text-center md:border-r  md:border-r-yellow-200  flex flex-col items-center justify-center">
-                <div>
-                <h1 className=" text-3xl font-bold w-[15rem]">400+</h1>
-                <h1 className="text-violet-100"> Customers</h1>
+                <div className="">
+                <h1 className=" text-3xl font-bold w-[15rem]">100+</h1>
+                <h1 className="text-violet-100 font-thin">Daily Services</h1>
                 </div>
             </div>
 
             <div className=" w-full h-[5rem]  text-center  flex flex-col items-center justify-center">
-                <div>
-                <h1 className="text-3xl font-bold w-[15rem]">4500+</h1>
-                <h1 className="text-violet-100 ">Happy Customers</h1>
+                <div className="     ">
+                <h1 className="text-3xl font-bold w-[15rem]">100+</h1>
+                <h1 className="text-violet-100 font-thin">Bike Accessories</h1>
                 </div>
             </div>
            
