ab2028a27dc336b5a7c2e8651cf979c69aac3ed2
Deepak <deepak2603dk@gmail.com>
2024-06-04 01:55:06 +0530

    showing dummy data to home

--- src/app/_Components/bikeProd/BikeProd.tsx
+++ src/app/_Components/bikeProd/BikeProd.tsx
@@ -40,8 +40,34 @@ const BikeProd = () => {
         <div className="my-7 w-full md:px-[1rem] px-1  grid grid-cols-1  md:grid-cols-2 sm:grid-cols-2  lg:grid-cols-4 md:gap-8 gap-5 place-items-center">
           {/* CARDS */}
 
-        <Card/>
-          
+
+          <div  className="h-[24rem] max-w-full w-[18rem]  bg-slate-900/25 backdrop-blur-md rounded-md   border-[1px] border-gray-800 hover:border-gray-700 border-b-gray-600 hover:border-b-gray-500  hover:-translate-y-2 transition ease-linear duration-300 ">
+            <div className="bg-slate-200 h-[15rem] flex items-center justify-center rounded-b-3xl">
+              <Image
+                className="hover:scale-110 overflow-hidden transition ease-linear duration-300 "
+                alt="product image"
+                src={
+                  "https://e-commerce-pied-xi.vercel.app/_next/image?url=%2Fimages%2Fproducts%2Fgamepad-red.webp&w=256&q=75"
+                }
+                width={100}
+                height={100}
+              />
+            </div>
+            {/* DETAIL */}
+            <div className="mt-3 px-5 flex flex-col gap-2">
+                <h1 className="text-md truncate  w-[90%]">prod name</h1>
+                
+              {/* Price */}
+              <h1 className="text-lg  font-serif text-blue-100">₹price</h1>
+              <div className="flex gap-1 items-center justify-start">
+              <div className="bg-violet-400 w-fit px-2 text-black rounded-full flex gap-1 items-center justify-start">
+                <h1 className="text-md">4.4</h1>
+                <Star size={20}/>
+              </div>
+              <h1 className="text-sm text-white/75">(321 Reviews)</h1>
+              </div>
+            </div>
+          </div>          
 
 
 
