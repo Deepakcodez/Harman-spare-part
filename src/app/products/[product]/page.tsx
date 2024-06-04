c2dfaf6aec25aa1deaac485deb6bd1fc35bf0030
Deepak <deepak2603dk@gmail.com>
2024-06-05 00:40:02 +0530

    api call for single product

--- src/app/products/[product]/page.tsx
+++ src/app/products/[product]/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+import Loader from "@/app/_Components/Shared/Loader/Loader";
+import Router from "next/router";
 import { FC } from "react";
+import useSWR from "swr";
+interface ProductProps {
+    params: {
+        product: string;
+    };
+}
 
-const  Product:FC= () => {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const  Product:FC<ProductProps>= ({params}) => {
+
+    const { data, error } = useSWR(`https://harman-spare-parts-backend.vercel.app/api/v1/product/product/${params.product}`, fetcher);
+
+    if (error) return <div>Failed to load</div>;
+    if (!data) return <div><Loader /></div>;
+    console.log('>>>>>>>>>>>', data.product)
+
     return ( 
         <>
         <div>
-            single prod
+            single prod {data.product.name}
         </div>
         </>
      );
