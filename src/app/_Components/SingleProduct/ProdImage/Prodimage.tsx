4227195e2eb68857666a7c61a1bff8090935e714
Deepak <deepak2603dk@gmail.com>
2024-07-12 02:17:48 +0530

    fixing small bug

--- src/app/_Components/SingleProduct/ProdImage/Prodimage.tsx
+++ src/app/_Components/SingleProduct/ProdImage/Prodimage.tsx
@@ -12,160 +12,174 @@ import useCurrentUser from "@/hooks/user/currentuser";
 import { useCartDetailStore } from "@/Store/CartCount/useCartDetail";
 
 interface ProdImageProps {
-  images: ImageType[];
-  productId: mongoose.Types.ObjectId;
+    images: ImageType[];
+    productId: mongoose.Types.ObjectId;
 }
 
 export interface AddProductToCartResponse {
-  success: boolean;
-  cart: CartDocument;
+    success: boolean;
+    cart: CartDocument;
 }
 
 export interface AddProductToCartData {
-  productId: mongoose.Types.ObjectId;
+    productId: mongoose.Types.ObjectId;
 }
 
 const ProdImage: FC<ProdImageProps> = ({ images, productId }) => {
-  const token = Cookies.get("HSPToken");
-  const prodimages = [1, 1, 1, , 1, 1]; // Placeholder for product images
-  const [isProductExistInCart, setIsProductExistInCart] = useState<boolean>(false);
-  const [isAddingToCart, setIsAddingToCart] = useState<boolean>(false);
-  const { data: currentUser, isLoading, isError, error } = useCurrentUser();
-  const router = useRouter();
+    const token = Cookies.get("HSPToken");
+    const prodimages = [1, 1, 1, , 1, 1]; // Placeholder for product images
+    const [isProductExistInCart, setIsProductExistInCart] = useState<boolean>(false);
+    const [isAddingToCart, setIsAddingToCart] = useState<boolean>(false);
+    const { data: currentUser, isLoading, isError, error } = useCurrentUser();
+    const router = useRouter();
 
-  const { increaseCartCount, decreaseCartCount } = useCartDetailStore();
+    const { increaseCartCount, decreaseCartCount } = useCartDetailStore();
 
-  const fetchCart = async () => {
-    if (currentUser) {
-      try {
-        const resp = await axios.get(
-          `https://harman-spare-parts-backend.vercel.app/api/v1/cart/details`,
-          {
-            headers: {
-              Authorization: token,
-            },
-          }
-        );
-        const cart = resp.data.cart;
-        const productExists = cart.products.some(
-          (p: any) => p.productId._id.toString() === productId.toString()
-        );
+    const fetchCart = async () => {
+        if (currentUser) {
+            try {
+                const resp = await axios.get(
+                    `https://harman-spare-parts-backend.vercel.app/api/v1/cart/details`,
+                    {
+                        headers: {
+                            Authorization: token,
+                        },
+                    }
+                );
+                const cart = resp.data.cart;
+                const productExists = cart.products.some(
+                    (p: any) => p.productId._id.toString() === productId.toString()
+                );
 
-        setIsProductExistInCart(productExists);
-      } catch (error) {
-        console.error("Error fetching cart:", error);
-      }
-    }
-  };
-
-  useEffect(() => {
-    fetchCart();
-  }, [productId, currentUser, isLoading, isError, error]);
-
-  const debouncedHandleAddToCart = async () => {
-    if (!isAddingToCart) {
-      setIsAddingToCart(true);
-      const data: AddProductToCartData = { productId };
-
-      if (!currentUser) {
-        router.push("/auth/login");
-      }
-
-      try {
-        const response = await axios.post<AddProductToCartResponse>(
-          "https://harman-spare-parts-backend.vercel.app/api/v1/cart/add",
-          data,
-          {
-            headers: { Authorization: token },
-          }
-        );
-        fetchCart();
-        increaseCartCount();
-        toast.success("Product added to cart");
-      } catch (error) {
-        setIsAddingToCart(false);
-        toast.error("Something Went Wrong");
-      }
-    }
-  };
-
-  const handleRemoveFromCart = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/api/v1/cart/remove",
-        { productId },
-        {
-          headers: { Authorization: token },
+                setIsProductExistInCart(productExists);
+            } catch (error) {
+                console.error("Error fetching cart:", error);
+            }
         }
-      );
-      fetchCart();
-      decreaseCartCount();
-      toast.success("Product removed from cart");
-    } catch (error) {
-      toast.error("Something Went Wrong");
-    }
-  };
+    };
 
-  const handleAddToCart = () => {
-    if (isProductExistInCart) {
-      handleRemoveFromCart();
-    } else {
-      debouncedHandleAddToCart();
-    }
-    setIsProductExistInCart(!isProductExistInCart);
-  };
+    useEffect(() => {
+        fetchCart();
+    }, [productId, currentUser, isLoading, isError, error]);
 
-  return (
-    <>
-      <div className="mt-[7rem]">
-        {/* main image */}
-        <div className="h-fit w-full flex justify-center">
-          <div className="h-auto px-3">
-            <Image
-              className="h-auto md:w-[40vw] w-[100vw] rounded-sm hover:scale-105 transition ease-linear duration-300"
-              src={"/bike4.jpg"}
-              width={500}
-              height={200}
-              alt="Prod image"
-            />
-          </div>
-        </div>
+    const debouncedHandleAddToCart = async () => {
+      
+            // setIsAddingToCart(true);
+            const data: AddProductToCartData = { productId };
 
-        {/* option images */}
-        <div>
-          <div className="flex justify-center py-3 gap-2 flex-wrap px-2">
-            {prodimages.map((image, index) => (
-              <Fragment key={index}>
-                <Image
-                  className="border cursor-pointer rounded-md"
-                  src={"/bike4.jpg"}
-                  width={50}
-                  height={50}
-                  alt="Prod image"
-                />
-              </Fragment>
-            ))}
-          </div>
+            if (!currentUser) {
+                router.push("/auth/login");
+            }
 
-          {/* buttons */}
-          <div className="w-full flex gap-9 justify-center mt-2">
-            <button
-              onClick={handleAddToCart}
-              className={`border-2 p-2 rounded-md transition ease-linear duration-300 ${!isProductExistInCart
-                ? "bg-violet-900 border-violet-700 text-white"
-                : "bg-gray-400 text-white/75"
-                }`}
-            >
-              {isProductExistInCart ? "Remove from cart" : "Add to Cart"}
-            </button>
-            <button className="border-2 border-violet-800 hover:bg-violet-900 hover:text-white p-2 rounded-md dark:text-black transition ease-linear duration-300 px-5">
-              Buy now
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            try {
+                setIsProductExistInCart(true)
+                const response = await axios.post<AddProductToCartResponse>(
+                    "https://harman-spare-parts-backend.vercel.app/api/v1/cart/add",
+                    data,
+                    {
+                        headers: { Authorization: token },
+                    }
+                );
+                if (response.status !== 200) {
+                   
+                    setIsProductExistInCart(false)
+                }else{
+                    fetchCart();
+                    increaseCartCount();
+                }
+                console.log('>>>>>>>>>>>a', response)
+                toast.success("Product added to cart");
+            } catch (error) {
+                setIsAddingToCart(false);
+                toast.error("Something Went Wrong");
+            }
+        
+    };
+
+    const handleRemoveFromCart = async () => {
+        try {
+            setIsProductExistInCart(false)
+            const resp = await axios.post(
+                "http://localhost:8000/api/v1/cart/remove",
+                { productId },
+                {
+                    headers: { Authorization: token },
+                }
+            );
+            if (resp.status !== 200) {
+
+               setIsProductExistInCart(true)
+            }else{
+                decreaseCartCount();
+                fetchCart();
+            }
+            console.log('>>>>>>>>>>>r', resp)
+            toast.success("Product removed from cart");
+        } catch (error) {
+            toast.error("Something Went Wrong");
+        }
+    };
+
+    const handleAddToCart = () => {
+        if (isProductExistInCart) {
+            handleRemoveFromCart();
+        } else {
+            debouncedHandleAddToCart();
+        }
+       
+    };
+
+    return (
+        <>
+            <div className="mt-[7rem]">
+                {/* main image */}
+                <div className="h-fit w-full flex justify-center">
+                    <div className="h-auto px-3">
+                        <Image
+                            className="h-auto md:w-[40vw] w-[100vw] rounded-sm hover:scale-105 transition ease-linear duration-300"
+                            src={"/bike4.jpg"}
+                            width={500}
+                            height={200}
+                            alt="Prod image"
+                        />
+                    </div>
+                </div>
+
+                {/* option images */}
+                <div>
+                    <div className="flex justify-center py-3 gap-2 flex-wrap px-2">
+                        {prodimages.map((image, index) => (
+                            <Fragment key={index}>
+                                <Image
+                                    className="border cursor-pointer rounded-md"
+                                    src={"/bike4.jpg"}
+                                    width={50}
+                                    height={50}
+                                    alt="Prod image"
+                                />
+                            </Fragment>
+                        ))}
+                    </div>
+
+                    {/* buttons */}
+                    <div className="w-full flex gap-9 justify-center mt-2">
+                        <button
+                            onClick={handleAddToCart}
+                            className={`border-2 p-2 rounded-md transition ease-linear duration-300 ${!isProductExistInCart
+                                ? "bg-violet-900 border-violet-700 text-white"
+                                : "bg-gray-400 text-white/75"
+                                }`}
+                        >
+                            {isProductExistInCart ? "Remove from cart" : "Add to Cart"}
+                        </button>
+                        <button className="border-2 border-violet-800 hover:bg-violet-900 hover:text-white p-2 rounded-md dark:text-black transition ease-linear duration-300 px-5">
+                            Buy now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ProdImage;
