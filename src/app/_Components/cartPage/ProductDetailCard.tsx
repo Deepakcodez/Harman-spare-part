df2c659b3ae92179b6c74d79685f66cb21647ec8
Deepak <deepak2603dk@gmail.com>
2024-07-14 06:22:25 +0530

    add prod quntity decreser functionality

--- src/app/_Components/cartPage/ProductDetailCard.tsx
+++ src/app/_Components/cartPage/ProductDetailCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 import useCartdetail from "@/hooks/cart/cartDetail";
+import { decreaseProdQuantity } from "@/services/cart/decreaseProdCartQuantity";
 import { addProductToCart } from "@/services/cart/increaseProdQuantity";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
@@ -13,20 +14,28 @@ export const ProductDetailCard = () => {
 
     const queryClient = useQueryClient()
 
-    const quantityAdder =(productId:string)=>{
+    const quantityAdder = (productId: string) => {
 
-        if(!productId) return toast.error("product ID missing");
+        if (!productId) return toast.error("product ID missing");
 
         mutation.mutate(productId)
     }
 
+    const quantityDecreaser = (productId: string) => {
+
+        if (!productId) return toast.error("product ID missing");
+
+        decreaseQuantity.mutate(productId)
+    }
+
+
 
     const mutation = useMutation({
-        mutationFn: (productId : string) => addProductToCart(productId),
+        mutationFn: (productId: string) => addProductToCart(productId),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['cartDetails'] })
-            // dispatch(toggleIsShown())
-           
+
+
             toast.success("quantity Added");
         },
         onError: () => {
@@ -34,50 +43,67 @@ export const ProductDetailCard = () => {
         }
     })
 
-   
+    const decreaseQuantity = useMutation({
+        mutationFn: (productId: string) => decreaseProdQuantity(productId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['cartDetails'] })
 
 
-  if(isLoading) return <div className="text-black">Loading</div>
-  if(error) return <div className="text-black">Error</div>
+            toast.success("quantity decreses");
+        },
+        onError: () => {
+            toast.error("Something went wrong")
+        }
+    })
+
+
+
+
+    if (isLoading) return <div className="text-black">Loading</div>
+    if (error) return <div className="text-black">Error</div>
     return (
         <>
             {
 
-                    cartProduct.products?.map((item: any, index: number) =>
-                        <Fragment key={index}>
-                            <div className=" py-2">
-                                <div className="border-t-2 flex justify-end p-1">
-                                    <RxCross1 />
-                                </div>
-                                {/* detail div */}
-                                <div className=" flex md:gap-4 gap-1">
-                                    <Image className="bg-violet-50 p-2 rounded-lg shadow-md h-[8rem]  w-[8rem] md:h-[10rem] md:w-[10rem]" src={"/rocket.png"} width={150} height={150} alt="ProductImage" />
-                                    <div className="flex flex-col gap-2">
-                                        <h1 className="text-lg md:text-2xl  ">
-                                            {item.product.productId.name}
-                                        </h1>
-                                        <div className="text-black">{JSON.stringify(item.product.productId._id)}</div>
-                                        <h1 className="text-sm">
-                                            {
-                                                item.product.productId.stock > 0 ?
-                                                    <div className="text-green-600">In Stock</div>
-                                                    :
-                                                    <div className="text-red-600">Out Of Stock</div>}
-                                        </h1>
-                                        <div className="border-2 rounded-lg justify-between flex w-[8rem]">
-                                            <button className="hover:bg-violet-100 w-12 rounded-s-lg">-</button>
-                                            <div>{item.product.prodQuantity}</div>
-                                            <button
-                                            onClick={()=>quantityAdder(item.product.productId._id)}
-                                                className="hover:bg-violet-100 w-12 rounded-e-lg">
-                                                +
-                                            </button>
-                                        </div>
-                                            
+                cartProduct.products?.map((item: any, index: number) =>
+                    <Fragment key={index}>
+                        <div className=" py-2">
+                            <div className="border-t-2 flex justify-end p-1">
+                                <RxCross1 />
+                            </div>
+                            {/* detail div */}
+                            <div className=" flex md:gap-4 gap-1">
+                                <Image className="bg-violet-50 p-2 rounded-lg shadow-md h-[8rem]  w-[8rem] md:h-[10rem] md:w-[10rem]" src={"/rocket.png"} width={150} height={150} alt="ProductImage" />
+                                <div className="flex flex-col gap-2">
+                                    <h1 className="text-lg   ">
+                                        {item.product.productId.name}
+                                    </h1>
+                                    <h1 className="text-sm">
+                                        {
+                                            item.product.productId.stock > 0 ?
+                                                <div className="text-green-600">In Stock</div>
+                                                :
+                                                <div className="text-red-600">Out Of Stock</div>}
+                                    </h1>
+                                    <div className="border-2 rounded-lg justify-between flex w-[8rem]">
+                                        <button
+                                            onClick={() => quantityDecreaser(item.product.productId._id)}
+                                            className="hover:bg-violet-100 w-12 rounded-s-lg">
+                                            -
+                                        </button>
+                                        <div>{item.product.prodQuantity}</div>
+                                        <button
+                                            onClick={() => quantityAdder(item.product.productId._id)}
+                                            className="hover:bg-violet-100 w-12 rounded-e-lg">
+                                            +
+                                        </button>
                                     </div>
+                                    <h1>₹{item.product.productId.price}</h1>
+
                                 </div>
                             </div>
-                        </Fragment>)
+                        </div>
+                    </Fragment>)
             }
         </>
     )
